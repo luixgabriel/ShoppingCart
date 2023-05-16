b68a9e87f03c59ec067b5e718cf1aa9ec7d48d07
unknown <luisgasbriel@gmail.com>
2023-05-16 15:07:25 -0300

    Finishing Project

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import {createContext ,useContext, useState} from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 import ShoppingCart from '../components/ShoppingCart';
 
 type ShoppingCartContext = {
@@ -29,7 +30,7 @@ export function useShoppingCart(){
 
 export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
 
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>('shopping-cart',[]);
     const [isOpen, setIsOpen] = useState(false);
 
     const cartQuantity = cartItems.reduce(
