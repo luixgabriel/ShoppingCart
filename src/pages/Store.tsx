16a968f68806d6052fea082f597e3b4279233aeb
unknown <luisgasbriel@gmail.com>
2023-05-16 09:30:32 -0300

    Advancing

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,21 @@
+import storeItens from '../data/itens.json';
+import {Col, Row} from 'react-bootstrap';
+import StoreItem from '../components/StoreItem';
+
 const Store = () => {
   return (
-    <div>
+    <>
       <h1>Store</h1>
-    </div>
+      {/* MD MEDIUM, XS XSMALL, LG LARGE */}
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItens.map((item)=>(
+          <Col key={item.id}>
+            <StoreItem {...item}/>
+          </Col>
+        ))}
+        
+      </Row>
+    </>
   )
 }
 
